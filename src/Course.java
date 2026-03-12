c39e463573e985bb66357e613d5f97195c627396
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-03-12 14:55:40 -0400

    Starter structure for course class

--- src/Course.java
+++ src/Course.java
@@ -1,3 +1,28 @@
 public class Course {
-    
+    private String courseCode;
+    private String courseName;
+    private String instructorId;
+
+    public Course(String courseCode, String courseName, String instructorId) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.instructorId = instructorId;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    @Override
+    public String toString() {
+        return courseCode + " - " + courseName + " (Instructor: " + instructorId + ")";
+    }
 }
