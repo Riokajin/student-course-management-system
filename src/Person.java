6400d1694af360209f47e15193662cc5b4151b91
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-03-12 14:33:59 -0400

    created overall structure and added class, fields, constructor getters and setters for person.java

---
+++ src/Person.java
@@ -0,0 +1,38 @@
+public abstract class Person {
+    private String name;
+    private String id;
+    private String email;
+
+
+    public Person(String name, String id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name; // This will be updated by validation
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id; // This will be updated by validation
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email; // This will be updated by validation
+    }
+
+    public abstract String getDetails();
+}
