e5b394cfa21074f90758e7fe604348cd574387cc
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-03-12 14:41:18 -0400

    starter structure for student.java

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,36 @@
-public class Student {
-    
+import java.util.ArrayList;
+
+public class Student extends Person {
+    private ArrayList<String> courses;
+    private String program;
+    private int yearLevel;
+
+    public Student(String name, String id, String email, String program, int yearLevel) {
+        super(name, id, email);
+        this.program = program;
+        this.yearLevel = yearLevel;
+        this.courses = new ArrayList<>();
+    }
+
+    public void addCourse(String courseCode) {
+        courses.add(courseCode);
+    }
+
+    public void removeCourse(String courseCode) {
+        courses.remove(courseCode);
+    }
+
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Student: " + getName() +
+               "\nID: " + getID() +
+               "\nEmail: " + getEmail() +
+               "\nProgram: " + program +
+               "\nYear Level: " + yearLevel;
+               "\nCourses: " + courses;
+    }
 }
