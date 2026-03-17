fa82abf79a42994dd6c700e519953f4a3d5bcdd6
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-03-17 18:12:50 -0400

    Integrated validation-exceptions changes into main manually (GitHub PR could not auto-merge)

--- src/Instructor.java
+++ src/Instructor.java
@@ -3,14 +3,36 @@ public class Instructor extends Person {
 
     public Instructor(String name, String id, String email, String department) {
         super(name, id, email);
-        this.department = department;
+        setDepartment(department);
+    }
+
+    public void setDepartment(String department) {
+        try {
+            if (department.trim() == null || department.trim().equals("")) {
+                System.out.println("Error setting Department: Department cannot be empty.");
+            }
+            else {
+                String departmentRegex = "^[A-Za-z0-9\\s]+$";
+                if (department.matches(departmentRegex)) {
+                    this.department = department;
+                    System.out.println("Department set successfully.");
+                }
+                else {
+                    System.out.println(
+                            "Error setting Department: Department name contains invalid characters (only use letters, numbers, and spaces)");
+                }
+            }
+        }
+        catch (NullPointerException e) {
+            System.out.println("Error setting Department: Department cannot be empty.");
+        }
     }
 
     @Override
     public String getDetails() {
         return "Instructor: " + getName() +
-               "\nID: " + getId() +
-               "\nEmail: " + getEmail() +
-               "\nDepartment: " + department;
+                "\nID: " + getId() +
+                "\nEmail: " + getEmail() +
+                "\nDepartment: " + department;
     }
 }
